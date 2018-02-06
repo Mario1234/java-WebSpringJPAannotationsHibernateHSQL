1eecc7fa9e8fda15819e3825818dbb1919bf2fe3
Mario1234 <30897590+Mario1234@users.noreply.github.com>
2018-02-06 18:54:17 +0100

    todo anotado

--- src/main/java/comun/configuracion/ConfiguracionHibernate.java
+++ src/main/java/comun/configuracion/ConfiguracionHibernate.java
@@ -1,4 +1,6 @@
 package comun.configuracion;
+import java.util.Properties;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
@@ -14,7 +16,9 @@ public class ConfiguracionHibernate {
 	@Bean
 	public LocalEntityManagerFactoryBean dameAdministradorEntidadesLocal() {
 		LocalEntityManagerFactoryBean adminEntLocal = new LocalEntityManagerFactoryBean();
+		adminEntLocal.setPersistenceProviderClass(org.hibernate.jpa.HibernatePersistenceProvider.class);
 		adminEntLocal.setPersistenceUnitName("PERSISTENCIA_LOCAL");
+		adminEntLocal.setJpaProperties(getHibernateProperties());
 		return adminEntLocal;
 	}
 
@@ -24,4 +28,18 @@ public class ConfiguracionHibernate {
 		administradorTransacciones.setEntityManagerFactory(dameAdministradorEntidadesLocal().getObject());
 		return administradorTransacciones;
 	}
+	
+	@Bean
+    public Properties getHibernateProperties()
+    {
+		Properties propiedadesPersistence = new Properties();		
+		propiedadesPersistence.put("javax.persistence.jdbc.url","jdbc:hsqldb:file:C:\\hsqldb\\");
+		propiedadesPersistence.put("javax.persistence.jdbc.user","SA");
+		propiedadesPersistence.put("javax.persistence.jdbc.password","");
+		propiedadesPersistence.put("javax.persistence.jdbc.driver","org.hsqldb.jdbcDriver");
+		propiedadesPersistence.put("hibernate.dialect","org.hibernate.dialect.HSQLDialect");
+		propiedadesPersistence.put("hibernate.show_sql","true");
+		propiedadesPersistence.put("hibernate.hbm2ddl.auto","update");
+		return propiedadesPersistence;
+    }
 }
