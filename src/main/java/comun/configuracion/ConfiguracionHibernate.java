5fb39a71db0c59457f8ad5ada83490085c8d5529
Mario1234 <30897590+Mario1234@users.noreply.github.com>
2018-02-23 09:11:53 +0100

    paquetes escanear necesarios si no hay persistence.xml
    paquetes escanear necesarios si no hay persistence.xml

--- src/main/java/comun/configuracion/ConfiguracionHibernate.java
+++ src/main/java/comun/configuracion/ConfiguracionHibernate.java
@@ -1,22 +1,38 @@
 package comun.configuracion;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScans(value = { @ComponentScan("comun")})
+@ComponentScans(value = { @ComponentScan("comun.usuario.*")})
 public class ConfiguracionHibernate {
+	
+   @Bean
+   public DataSource fuenteDatos(){
+      DriverManagerDataSource fuenteDatos = new DriverManagerDataSource();
+      fuenteDatos.setDriverClassName("org.hsqldb.jdbcDriver");
+      fuenteDatos.setUrl("jdbc:hsqldb:file:C:\\hsqldb\\");
+      fuenteDatos.setUsername( "SA" );
+      fuenteDatos.setPassword( "" );
+      return fuenteDatos;
+   }
+	
 	@Bean
-	public LocalEntityManagerFactoryBean dameAdministradorEntidadesLocal() {
-		LocalEntityManagerFactoryBean adminEntLocal = new LocalEntityManagerFactoryBean();
+	public LocalContainerEntityManagerFactoryBean dameAdministradorEntidadesLocal() {
+		LocalContainerEntityManagerFactoryBean  adminEntLocal = new LocalContainerEntityManagerFactoryBean();
 		adminEntLocal.setPersistenceProviderClass(org.hibernate.jpa.HibernatePersistenceProvider.class);
+		adminEntLocal.setDataSource(fuenteDatos());
+		adminEntLocal.setPackagesToScan("comun.usuario.*");
 		adminEntLocal.setPersistenceUnitName("PERSISTENCIA_LOCAL");
 		adminEntLocal.setJpaProperties(getHibernateProperties());
 		return adminEntLocal;
@@ -33,10 +49,6 @@ public class ConfiguracionHibernate {
     public Properties getHibernateProperties()
     {
 		Properties propiedadesPersistence = new Properties();		
-		propiedadesPersistence.put("javax.persistence.jdbc.url","jdbc:hsqldb:file:C:\\hsqldb\\");
-		propiedadesPersistence.put("javax.persistence.jdbc.user","SA");
-		propiedadesPersistence.put("javax.persistence.jdbc.password","");
-		propiedadesPersistence.put("javax.persistence.jdbc.driver","org.hsqldb.jdbcDriver");
 		propiedadesPersistence.put("hibernate.dialect","org.hibernate.dialect.HSQLDialect");
 		propiedadesPersistence.put("hibernate.show_sql","true");
 		propiedadesPersistence.put("hibernate.hbm2ddl.auto","update");
